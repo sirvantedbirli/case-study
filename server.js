ddcf90526f494e05630b354ae3297a26ee627bd2
sirvantedbirli <tedbirlisirvan@icloud.com>
2025-08-28 04:11:26 +0300

    Fix port configuration for Azure

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 require("./app/routes/customer.routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
